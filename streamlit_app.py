a3153937b77131cfe66a0d55f2ea2dff4b52ab03
Leon-cloud-create <leonj062712@gmail.com>
2025-12-21 15:13:36 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,7 @@ PATIENTS_CSV = "patients.csv"
 MODEL_PATH = "model.pkl"
 SCALER_PATH = "scaler.pkl"
 
-SCAN_MODEL_PATH = "Neuroblastoma.keras"  
+SCAN_MODEL_PATH = "NeuroM.keras"  
 
 # ---------------- Translations (FIXED - COMPLETE) ----------------
 translations = {
